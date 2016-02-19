f28fc83f1bfbcc0aa85daae0aef9d733461e53e7
firstvp <piratik45@yandex.ru>
2016-02-19 23:30:24 +0300

    Initial Commint

---
+++ Task1/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Task1
+{
+    abstract class Shape
+    {
+        protected Point CenterPoint;
+
+        protected Shape(Point CenterPoint)
+        {
+            this.CenterPoint = CenterPoint;
+        }
+
+        protected abstract void Draw();
+    }
+   
+}
